80bc398fdb787b915d5c43af234274a2903f0f3a
Maria Fernanda <85709697+flanconi@users.noreply.github.com>
2022-02-18 16:47:10 -0300

    Verificar ENDPOINT Postman

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,15 @@ import { criarDocente } from "./endpoints/docentes/criarDocente"
 import { mudarTurmaDocente } from "./endpoints/docentes/mudarTurmaDocente"
 import { buscarTurmaAtiva } from "./endpoints/turma/buscarTurmaAtiva"
 import { criarTurma } from "./endpoints/turma/criarTurma"
+import { criarEstudante } from "./endpoints/estudantes/criarEstudante"
 
 app.get("/docente", pegarDocentes)
 app.get("/turma", buscarTurmaAtiva)
 app.post("/docente/criar", criarDocente)
 app.post("/turma/criar", criarTurma)
 app.put("/docente/editar", mudarTurmaDocente)
+
+//Alunos 
+
+app.post('/alunos', criarEstudante)
 
